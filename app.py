b68de3a606f0edc020b2166fc3380db97b96cbc1
oni-97 <oni.ils.thou@gmail.com>
2022-04-03 16:46:33 +0900

    listen to messange and respond to it

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
+@app.message("hello")
+def message_hello(message, say):
+    say(f"Hey there <@{message['user']}>!")
+
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
