5cffb6ccc51daaffe45ce77f60077817c15e9621
s-onizuk <s-onizuk@sel-samba.ics.es.osaka-u.ac.jp>
2022-04-07 15:39:32 +0900

    add support for select from db

--- database/mysql_util.py
+++ database/mysql_util.py
@@ -1,4 +1,4 @@
-from database.mysql import execute_sql
+from mysql import execute_sql_and_commit, execute_sql_and_fetchall
 
 
 def insert_purchase_data(user_id, amount):
@@ -6,7 +6,7 @@ def insert_purchase_data(user_id, amount):
         sql = (
             f"insert into purchase_data(user_id, amount)values('{user_id}', '{amount}')"
         )
-        execute_sql(sql)
+        execute_sql_and_commit(sql)
         return True
     except Exception as e:
         print(e)
@@ -18,8 +18,28 @@ def insert_payment_data(user_id, amount):
         sql = (
             f"insert into payment_data(user_id, amount)values('{user_id}', '{amount}')"
         )
-        execute_sql(sql)
+        execute_sql_and_commit(sql)
         return True
     except Exception as e:
         print(e)
         return False
+
+
+def select_purchase_data(user_id):
+    try:
+        sql = f"SELECT `amount` FROM `purchase_data` WHERE `user_id`='{user_id}'"
+        result = execute_sql_and_fetchall(sql)
+        return result
+    except Exception as e:
+        print(e)
+        return None
+
+
+def select_payment_data(user_id):
+    try:
+        sql = f"SELECT `amount` FROM `payment_data` WHERE `user_id`='{user_id}'"
+        result = execute_sql_and_fetchall(sql)
+        return result
+    except Exception as e:
+        print(e)
+        return None
