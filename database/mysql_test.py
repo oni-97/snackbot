e93829d7cf9765692133f04ce04f97aaf77e2352
s-onizuk <s-onizuk@sel-samba.ics.es.osaka-u.ac.jp>
2022-04-07 13:09:58 +0900

    add support for insert_purchase_data to db

--- database/mysql_test.py
+++ database/mysql_test.py
@@ -1,11 +1,10 @@
-from mysql import MySQL
+from mysql import execute_sql
 
 
 try:
-    mysql = MySQL("snackbot_db")
-    sql = "insert into purchase_data(user_id, amount)values('age23', 76431)"
-    mysql.executal(sql)
+    sql = "insert into purchase_data(user_id, amount)values('age23', 35)"
+    execute_sql(sql)
     sql = "insert into purchase_data(user_id, amount)values('dge24', -187)"
-    mysql.executal(sql)
+    execute_sql(sql)
 except Exception as e:
     print(e)
