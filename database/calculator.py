963a49d538f09059692845952fdf0d94ba2adb7f
oni-97 <oni.ils.thou@gmail.com>
2022-04-07 19:07:54 +0900

    add support for transaction and db error handling

--- database/calculator.py
+++ database/calculator.py
@@ -1,18 +1,21 @@
-from database.mysql_util import select_payment_data, select_purchase_data
+from database.mysql_util import select_data
 
 
 def total_amount_of_payment(user_id):
-    pay_data = select_payment_data(user_id)
-    total = 0
-    for data in pay_data:
-        total += data["amount"]
-    return total
+    return caluculate_total_amount(user_id=user_id, table_name="payment_data")
 
 
 def total_amount_of_purchase(user_id):
-    pay_data = select_purchase_data(user_id)
+    return caluculate_total_amount(user_id=user_id, table_name="purchase_data")
+
+
+def caluculate_total_amount(user_id, table_name):
+    data_list = select_data(user_id=user_id, table_name=table_name)
+    if data_list is None:
+        return None
+
     total = 0
-    for data in pay_data:
+    for data in data_list:
         total += data["amount"]
     return total
 
@@ -20,4 +23,6 @@ def total_amount_of_purchase(user_id):
 def unpaid_amount(user_id):
     payment_amount = total_amount_of_payment(user_id=user_id)
     purchase_amount = total_amount_of_purchase(user_id=user_id)
+    if (payment_amount is None) or (purchase_amount is None):
+        return None
     return purchase_amount - payment_amount
